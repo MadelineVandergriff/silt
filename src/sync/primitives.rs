0fbd526cbb4900641ec91276e90ebe0009c69478
JackVandergriff <jackvandergriff02@gmail.com>
2023-04-24 22:09:17 -0400

    Added semaphores

--- src/sync/primitives.rs
+++ src/sync/primitives.rs
@@ -14,6 +14,28 @@ impl Destructible for SyncPrimitives {
     }
 }
 
-pub fn get_sync_primitives(loader: &Loader) -> ParitySet<SyncPrimitives> {
-    panic!()
+pub unsafe fn get_sync_primitives(loader: &Loader) -> ParitySet<SyncPrimitives> {
+    ParitySet::from_fn(|| {
+        let image_available = loader
+            .device
+            .create_semaphore(&vk::SemaphoreCreateInfo::default(), None)
+            .unwrap();
+        let render_finished = loader
+            .device
+            .create_semaphore(&vk::SemaphoreCreateInfo::default(), None)
+            .unwrap();
+
+        let fence_create_info =
+            vk::FenceCreateInfo::builder().flags(vk::FenceCreateFlags::SIGNALED);
+        let in_flight = loader
+            .device
+            .create_fence(&fence_create_info, None)
+            .unwrap();
+
+        SyncPrimitives {
+            image_available,
+            render_finished,
+            in_flight,
+        }
+    })
 }
